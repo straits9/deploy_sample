33cbf45cce2564536e035867ea9443f8c36c595e
straits9 <straits9@users.noreply.github.com>
2025-09-13 08:10:57 +0000

    deploy: 480df2ae9c662c868e490cad66dc1804d19283c8

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "1ef3ea3a0fec4569e5d531da25f34095",
 "assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
-"flutter_bootstrap.js": "74de624aae08e0068d42daf94921add5",
+"flutter_bootstrap.js": "12cdc08591e6f9188dcdac9d8363dab8",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "index.html": "b5fbf4e8915eb672bad28e19a93b9c33",
 "/": "b5fbf4e8915eb672bad28e19a93b9c33"};
